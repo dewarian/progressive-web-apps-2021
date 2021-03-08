9a08bd1b232be06aa510c4a17548bdae75c6ab3c
Nathan <nathanbommezijn@gmail.com>
2021-03-08 17:29:41 +0100

    :fire: Removal eslint & prettier
    Currently ESLINT and Prettier arent working together

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express');
+const app = express();
+const port = 8080;
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.send('hello world')
-})
+  res.send('hello world');
+});
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
-})
+  console.log(`App listening at http://localhost:${port}`);
+});
+          console.log('test')
