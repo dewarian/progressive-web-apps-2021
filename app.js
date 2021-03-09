588e08a92d62f47d6d9c1083144d4f121ae2086c
Nathan <nathanbommezijn@gmail.com>
2021-03-09 16:21:52 +0100

    :bento: Setup src skeletal structure
    Setup the structure of the application to continue refactoring clientside to serverside

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 import express from 'express';
+// import path from 'path'
 const app = express();
 const port = 8080;
 
 app.set('view engine', 'ejs');
+app.set('views', 'src/views')
+
+// Custom path for static files
+app.use('/static', express.static('src/static'))
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.render('home', {
+    title: 'home'
+  })
 });
 
 app.listen(port, () => {
