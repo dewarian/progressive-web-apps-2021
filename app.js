0561afd1fcb7bf6aaae7e0c84c2a50f2f9f9a149
dewarian <nathanbommezijn@gmail.com>
2021-03-10 23:19:03 +0100

    :construction: Add API call handling

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
-import express from 'express';
-// import path from 'path'
+import express from "express";
+import getData from "handling-data";
 const app = express();
 const port = 8080;
 
-app.set('view engine', 'ejs');
-app.set('views', 'src/views')
+app.set("view engine", "ejs");
+app.set("views", "src/views");
 
 // Custom path for static files
-app.use('/static', express.static('src/static'))
+app.use("/static", express.static("src/static"));
+const year = 2021;
+const season = "winter";
+const offset = 0;
+const baseUrl = "https://kitsu.io/api/edge/anime";
+const seasonYearFilter = "[seasonYear]=";
+const seasonFilter = "[season]=";
+const pageLimit = "page[limit]=20";
+const offSet = "page[offset]=";
 
-app.get('/', (req, res) => {
-  res.render('home', {
-    title: 'home'
-  })
+app.get("/", async (req, res) => {
+  const dataset = await getData(
+    `${baseUrl}?filter${seasonYearFilter}${year}&${seasonFilter}${season}&${pageLimit}&${offSet}${offset}`
+  );
+  res.render("home", {
+    title: "home"
+  });
 });
 
 app.listen(port, () => {
---
+++ src/modules/handling-data.js
@@ -0,0 +1,21 @@
+/**
+ * @title Check module.data
+ * @function
+ * @description Test function
+ * @param {String} test A string to check if the modular approach works properly
+ * @return {String} log of test
+ */
+export const datatest = (test) => {
+  return console.log(`%c${test}`, "color: #ffcc00; font-weight: bold;");
+};
+/**
+ * @title Retrieve Data from API
+ * @function
+ * @description Data retrieval. Use getData when retrieving data from an external API.
+ * @param {String} url API endpoint to call.
+ */
+export const getData = async (url) => {
+  return await fetch(url)
+    .then((response) => response.json())
+    .catch((err) => console.log(err));
+};
