48fceec6a482328f8de6849a3cd352258cf95945
dewarian <nathanbommezijn@gmail.com>
2021-03-22 16:48:31 +0100

    :building_construction: Restructure project

--- scripts/build-css.js
+++ scripts/build-css.js
@@ -5,7 +5,7 @@ const output = new CleanCSS({
   level: {
     2: { all: false, removeDuplicateFontRules: true }
   }
-}).minify(["src/static/css/index.css"]);
+}).minify(["public/css/index.css"]);
 // console.log(output.styles);
 fs.writeFile("./dist/index.css", output.styles, (err) => {
   if (err) return console.log(err);
