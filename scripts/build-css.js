475f36785e0b8a09e94b51cb6d3edb2cd1043726
dewarian <nathanbommezijn@gmail.com>
2021-03-26 11:23:09 +0100

    :fire: Remove comments

--- scripts/build-css.js
+++ scripts/build-css.js
@@ -6,7 +6,6 @@ const output = new CleanCSS({
     2: { all: false, removeDuplicateFontRules: true }
   }
 }).minify(["public/css/index.css"]);
-// console.log(output.styles);
 fs.writeFile("./dist/index.css", output.styles, (err) => {
   if (err) return console.log(err);
   console.log(`Finished minifying CSS in ${output.stats.timeSpent}s`);
