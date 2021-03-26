4fdf9adade3dfc47e3c554cfb3e69b610868f78c
dewarian <nathanbommezijn@gmail.com>
2021-03-26 11:24:30 +0100

    :sparkles: Update basic min. variant of serviceworker

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,1 +1,18 @@
-console.log("hello");
+const cacheVersion = "caching-v1";
+const cacheURLs = ["index.html"];
+
+self.addEventListener("install", async (e) => {
+  console.log(`Service Worker: Installed`);
+  caches.open(cacheVersion).then((cache) => {
+    return cache.add("/index.html");
+  });
+  return self.skipWaiting();
+});
+
+// self.addEventListener("activate", () => {});
+
+/* 
+critical css in head, de rest in de body, critical inline style
+Termen van de presentaties even doornemen en begrijpen. zodat je weet awt het doet en als je het hebt toegepast.
+
+*/
