e8f890e3a78a189c603c00d6b9717e9ef922a479
dewarian <nathanbommezijn@gmail.com>
2021-03-30 12:39:59 +0200

    :poop: Basic version of serviceworker
    If the page is not available return an offline page.
    Currently I have an issue with the fetch event because I am catching the request for my images instead of throwing an offline

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -24,33 +24,50 @@ self.addEventListener("activate", (e) => {
   console.log(`kitsu-${cacheVersion} available`);
 });
 
-self.addEventListener("fetch", (e) => {
-  // console.log("neet", e.request.url);
-  e.respondWith(
-    fetch(e.request)
+// self.addEventListener("fetch", (e) => {
+//   // console.log("neet", e.request.url);
+//   e.respondWith(
+//     fetch(e.request)
+//       .then((response) => {
+//         const rs = response.clone();
+//         caches.open(`kitsu-${cacheVersion}`).then((cache) => {
+//           cache.put(e.request, rs);
+//         });
+//         return response;
+//       })
+//       .catch(() => {
+//         let isOffline = true;
+//         if (e.request.method === "GET") {
+//           caches.open(`kitsu-${cacheVersion}`).then((c) => {
+//             c.keys().then((r) => {
+//               r.forEach((p) => {
+//                 if (p.url.includes(e.request.url)) {
+//                   isOffline = false;
+//                 }
+//               });
+//             });
+//           });
+//         }
+//         if (isOffline) {
+//           return caches.match(e.request.url);
+//         }
+//       })
+//   );
+// });
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
       .then((response) => {
-        const rs = response.clone();
-        caches.open(`kitsu-${cacheVersion}`).then((cache) => {
-          cache.put(e.request, rs);
-        });
-        return response;
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
       })
-      .catch(() => {
-        let isOffline = true;
-        if (e.request.method === "GET") {
-          caches.open(`kitsu-${cacheVersion}`).then((c) => {
-            c.keys().then((r) => {
-              r.forEach((p) => {
-                if (p.url.includes(e.request.url)) {
-                  isOffline = false;
-                }
-              });
-            });
-          });
-        }
-        if (isOffline) {
-          return caches.match(e.request.url);
-        }
+      .catch((error) => {
+        return caches.match("/offline");
       })
   );
 });
+// check cache => on false make network request => on false return offline page
