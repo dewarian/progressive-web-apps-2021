72337fbbbd5a9823d8a464716e64154decc280a4
dewarian <nathanbommezijn@gmail.com>
2021-03-30 13:07:53 +0200

    :hammer: Update build script for images

--- scripts/build-static.js
+++ scripts/build-static.js
@@ -1,21 +1,24 @@
 const copy = require("copy");
 
-copy(
-  ["./public/*.js", "./public/*.json", "./public/kitsu.png"],
-  "dist",
-  function (err, files) {
-    if (err) {
-      throw err;
-    } else {
-      console.log(`copied: ${JSON.stringify(files.length)}`);
-    }
+copy(["./public/*.js", "./public/*.json"], "dist", function (err, files) {
+  if (err) {
+    throw err;
+  } else {
+    console.log(`copied: ${JSON.stringify(files.length)}`);
   }
-);
+});
 
 copy(["./public/fonts/*"], "dist/fonts", (err, files) => {
   if (err) {
     throw err;
   } else {
     console.log(`copied fonts`);
+  }
+});
+copy(["./public/img/*"], "dist/img", (err, files) => {
+  if (err) {
+    throw err;
+  } else {
+    console.log(`copied images`);
   }
 });
